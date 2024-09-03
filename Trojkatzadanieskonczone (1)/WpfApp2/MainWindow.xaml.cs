68d05db5d0ccc0e5aa547aed55d02ecb7d5dc588
Bartekszp <120750770+Bartekszp@users.noreply.github.com>
2024-09-03 20:43:08 +0200

    Add files via upload

---
+++ Trojkatzadanieskonczone (1)/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(bok1.Text);
+            int b = int.Parse(bok2.Text);
+            int c = int.Parse(bok3.Text);
+
+            double obwod = 0;
+            double pole = 0;
+            double p = 0;
+            string TypTrojkata = "";
+
+            if (a + b <= c || b + c <= a || c + a <= b)
+            {
+                return;
+            }
+            if ( a != b && b != c)
+            {
+                TypTrojkata = "roznoboczny";
+            }else if (a == b && b != c)
+            {
+                TypTrojkata = "rownoramienny";
+            }else if(a == b && b == c)
+            {
+                TypTrojkata = "rownoboczny";
+            }
+
+            obwod = a + b + c;
+            p = obwod / 2;
+            pole = Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+
+            lblWynik.Content = "a = " + a + " b = " + b + " c = " + c + "\n" + "Obwod wynosi: " + obwod + "\n" + "Pole Wynosi: " + Math.Round(pole,2) + "\n" + "Trojkat jest: " + TypTrojkata; 
+        }
+    }
+}
---
+++ skonczoneDaneHaslo/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string haslo = "";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string imie = txtBoxImie.Text;
+            string nazwisko = txtBoxNazwiko.Text;
+            string wybor = ComBox.Text;
+            if (imie == "" && nazwisko == "" && wybor == "")
+            {
+                MessageBox.Show("Uzupelnij dane");
+                return;
+            }
+            
+
+            MessageBox.Show($"Dane pracownika: {imie} {nazwisko} {wybor} haslo: {haslo}");
+        }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string male = "qwertyuiopasdfghjklzxcvbnm";
+            string duze = "QWERTYUIOPASDFGHJKLZXCVBNM";
+            string liczby = "1234567890";
+            string znaki = "!@#$%^&*()_+-=";
+            haslo = "";
+            int iloscZnakow;
+            if (int.TryParse(IloscZnakowlbl.Text, out iloscZnakow) == false)
+            {
+                MessageBox.Show("Podaj ilosc znakow");
+                return;
+            }
+
+
+            
+            Random rnd = new Random();
+
+            for (int i = 0; i < iloscZnakow; i++)
+            {
+                if (i == 0) haslo += male[rnd.Next(male.Length)];
+                else if(i == 1 && WielkieChecBox.IsChecked == true) haslo += duze[rnd.Next(duze.Length)];
+                else if(i == 2 && CyfryChecBox.IsChecked == true) haslo += liczby[rnd.Next(liczby.Length)];
+                else if(i == 3 && ZnakiChecBox.IsChecked == true) haslo += znaki[rnd.Next(znaki.Length)];
+                else haslo += male[rnd.Next(male.Length)];
+            }
+
+            MessageBox.Show(haslo);
+
+
+        }
+
+        private void WielkieChecBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
